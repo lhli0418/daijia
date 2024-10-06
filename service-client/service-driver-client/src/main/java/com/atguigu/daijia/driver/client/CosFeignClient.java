999dbf92391a86cc1d4d757f04c6793ff9732791
lehang <1430081225@qq.com>
2024-10-06 16:26:11 +0800

    4.4 腾讯云对象存储cos上传接口 driver-web端

--- service-client/service-driver-client/src/main/java/com/atguigu/daijia/driver/client/CosFeignClient.java
+++ service-client/service-driver-client/src/main/java/com/atguigu/daijia/driver/client/CosFeignClient.java
@@ -9,8 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.color.ICC_Profile;
+
 @FeignClient(value = "service-driver")
 public interface CosFeignClient {
 
 
+    /**
+     * 上传
+     * @param file
+     * @param path
+     * @return
+     */
+    @PostMapping(value = "/cos/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)//字节类型上传
+    Result<CosUploadVo> uploadFile(@RequestPart("file") MultipartFile file, @RequestParam("path") String path);
 }
--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CosServiceImpl.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CosServiceImpl.java
@@ -1,13 +1,31 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.driver.client.CosFeignClient;
 import com.atguigu.daijia.driver.service.CosService;
+import com.atguigu.daijia.model.vo.driver.CosUploadVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class CosServiceImpl implements CosService {
 
+    @Autowired
+    private CosFeignClient cosFeignClient;
+    /**
+     * 上传司机信息
+     * @param file
+     * @param path
+     * @return 返回上传vo对象
+     */
+    @Override
+    public CosUploadVo upload(MultipartFile file, String path) {
 
+        Result<CosUploadVo> cosUploadVoResult = cosFeignClient.uploadFile(file,path);
+        return cosUploadVoResult.getData();
+    }
 }
