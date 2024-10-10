5e8931b8756d641687baf2a385ba22836f241a0b
lehang <1430081225@qq.com>
2024-10-11 01:17:26 +0800

    4.13 乘客下单 保存订单信息 返回订单id

--- service/service-order/src/main/java/com/atguigu/daijia/order/controller/OrderInfoController.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/controller/OrderInfoController.java
@@ -1,8 +1,14 @@
 package com.atguigu.daijia.order.controller;
 
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.model.form.order.OrderInfoForm;
+import com.atguigu.daijia.order.mapper.OrderInfoMapper;
 import com.atguigu.daijia.order.service.OrderInfoService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/order/info")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderInfoController {
-	
+
+    @Autowired
+    private OrderInfoService orderInfoService;
+
+    /**
+     * 保存订单信息
+     * @param orderInfoForm 订单具体信息
+     * @return 返回订单id号
+     */
+    @Operation(summary = "保存订单信息")
+    @PostMapping("/saveOrderInfo")
+    public Result<Long> saveOrderInfo(@RequestBody OrderInfoForm orderInfoForm){
+        Long orderId = orderInfoService.saveOrderInfo(orderInfoForm);
+        return Result.ok(orderId);
+    }
 
 }
 
