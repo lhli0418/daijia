f8769cab46ae3e1f913ab79f90ab646275970181
lehang <1430081225@qq.com>
2024-10-13 15:16:30 +0800

    4.16 搜索附近司机

--- service-client/service-map-client/src/main/java/com/atguigu/daijia/map/client/MapFeignClient.java
+++ service-client/service-map-client/src/main/java/com/atguigu/daijia/map/client/MapFeignClient.java
@@ -2,10 +2,14 @@ package com.atguigu.daijia.map.client;
 
 import com.atguigu.daijia.common.result.Result;
 import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
+import com.atguigu.daijia.model.form.map.SearchNearByDriverForm;
 import com.atguigu.daijia.model.vo.map.DrivingLineVo;
+import com.atguigu.daijia.model.vo.map.NearByDriverVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(value = "service-map")
 public interface MapFeignClient {
@@ -18,4 +22,11 @@ public interface MapFeignClient {
     @PostMapping("/map/calculateDrivingLine")
     Result<DrivingLineVo> calculateDrivingLine(@RequestBody CalculateDrivingLineForm calculateDrivingLineForm);
 
+    /**
+     * 搜索附近满足条件的司机
+     * @param searchNearByDriverForm
+     * @return
+     */
+    @PostMapping("/map/location/searchNearByDriver")
+    Result<List<NearByDriverVo>> searchNearByDriver(@RequestBody SearchNearByDriverForm searchNearByDriverForm);
 }
--- service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/LocationServiceImpl.java
+++ service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/LocationServiceImpl.java
@@ -1,13 +1,28 @@
 package com.atguigu.daijia.map.service.impl;
 
 import com.atguigu.daijia.common.constant.RedisConstant;
+import com.atguigu.daijia.common.constant.SystemConstant;
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.driver.client.DriverInfoFeignClient;
 import com.atguigu.daijia.map.service.LocationService;
+import com.atguigu.daijia.model.entity.driver.DriverSet;
+import com.atguigu.daijia.model.form.map.SearchNearByDriverForm;
 import com.atguigu.daijia.model.form.map.UpdateDriverLocationForm;
+import com.atguigu.daijia.model.vo.map.NearByDriverVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.geo.Point;
+import org.springframework.data.geo.*;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -16,6 +31,9 @@ public class LocationServiceImpl implements LocationService {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private DriverInfoFeignClient driverInfoFeignClient;
     // 司机开启接单，更新司机位置信息
     @Override
     public Boolean updateDriverLocation(UpdateDriverLocationForm updateDriverLocationForm) {
@@ -34,5 +52,57 @@ public class LocationServiceImpl implements LocationService {
     public Boolean removeDriverLocation(Long driverId) {
         redisTemplate.opsForGeo().remove(RedisConstant.DRIVER_GEO_LOCATION,driverId.toString());
         return true;
+    }
+
+
+    /**
+     * 搜索附近满足条件的司机
+     * @param searchNearByDriverForm 司机经纬度 订单里程
+     * @return
+     */
+    @Override
+    public List<NearByDriverVo> searchNearByDriver(SearchNearByDriverForm searchNearByDriverForm) {
+        // 搜索五公里以内的司机
+        // 定义经纬度
+        Point point = new Point(searchNearByDriverForm.getLongitude().doubleValue(),searchNearByDriverForm.getLatitude().doubleValue());
+        // 定义范围 五公里（系统配置）
+        Distance distance = new Distance(SystemConstant.NEARBY_DRIVER_RADIUS,RedisGeoCommands.DistanceUnit.KILOMETERS);
+        // 定义以point点为中心，distance为距离这么一个范围
+        Circle circle = new Circle(point,distance);
+
+        // 定义GEO参数
+        RedisGeoCommands.GeoRadiusCommandArgs args = RedisGeoCommands.GeoRadiusCommandArgs.newGeoRadiusArgs()
+                                                                    .includeCoordinates() // 返回坐标
+                                                                    .includeDistance()    // 返回距离
+                                                                    .sortAscending();     // 排序：升序
+
+        // 1.GEORADIUS获取附近范围的信息
+        GeoResults<RedisGeoCommands.GeoLocation<String>> result = redisTemplate.opsForGeo().radius(RedisConstant.DRIVER_GEO_LOCATION, circle, args);
+        // 2.收集信息，存入list
+        List<GeoResult<RedisGeoCommands.GeoLocation<String>>> content = Objects.requireNonNull(result).getContent();
+        // 3.返回计算后的信息
+        List<NearByDriverVo> list = new ArrayList<>();
+        for (GeoResult<RedisGeoCommands.GeoLocation<String>> item : content) {
+            // 获取司机id
+            Long driverId = Long.parseLong(item.getContent().getName());
+            // 获取司机距离
+            BigDecimal currentDistance = BigDecimal.valueOf(item.getDistance().getValue()).setScale(2, RoundingMode.HALF_UP);
+            // 获取司机个性化设置
+            DriverSet driverSet = driverInfoFeignClient.getDriverSet(driverId).getData();
+            // 接单里程判断，acceptDistance==0：不限制，
+            if (driverSet.getAcceptDistance().doubleValue() != 0 && driverSet.getAcceptDistance().subtract(currentDistance).doubleValue() < 0) {
+                continue;
+            }
+            // 订单里程判断，orderDistance==0：不限制
+            if (driverSet.getOrderDistance().doubleValue() != 0 && driverSet.getOrderDistance().subtract(searchNearByDriverForm.getMileageDistance()).doubleValue() < 0) {
+                continue;
+            }
+            // 满足条件的附近司机信息
+            NearByDriverVo nearByDriverVo = new NearByDriverVo();
+            nearByDriverVo.setDriverId(driverId);
+            nearByDriverVo.setDistance(currentDistance);
+            list.add(nearByDriverVo);
+        }
+        return list;
     }
 }
