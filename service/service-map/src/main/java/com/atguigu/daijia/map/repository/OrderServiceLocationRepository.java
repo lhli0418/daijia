000c68a618fe55aa06a500348320454178b23cd1
lehang <1430081225@qq.com>
2024-10-22 22:42:39 +0800

    6.4.2 订单执行 开始代驾 批量保存订单位置信息

--- service/service-map/src/main/java/com/atguigu/daijia/map/repository/OrderServiceLocationRepository.java
+++ service/service-map/src/main/java/com/atguigu/daijia/map/repository/OrderServiceLocationRepository.java
@@ -4,6 +4,9 @@ import com.atguigu.daijia.model.entity.map.OrderServiceLocation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * mongoDB repository类
+ */
 @Repository
 public interface OrderServiceLocationRepository extends MongoRepository<OrderServiceLocation, String> {
 
