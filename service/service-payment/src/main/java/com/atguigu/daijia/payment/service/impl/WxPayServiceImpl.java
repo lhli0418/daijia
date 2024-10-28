c9eea56f021a69ddd6a265268d6290c53e226e78
lehang <1430081225@qq.com>
2024-10-28 20:33:12 +0800

    8.2.4 订单支付 微信支付 微信支付功能

--- service/service-payment/src/main/java/com/atguigu/daijia/payment/service/impl/WxPayServiceImpl.java
+++ service/service-payment/src/main/java/com/atguigu/daijia/payment/service/impl/WxPayServiceImpl.java
@@ -1,14 +1,94 @@
 package com.atguigu.daijia.payment.service.impl;
 
+import com.alibaba.fastjson2.JSON;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
+import com.atguigu.daijia.model.entity.payment.PaymentInfo;
+import com.atguigu.daijia.model.form.payment.PaymentInfoForm;
+import com.atguigu.daijia.model.vo.payment.WxPrepayVo;
+import com.atguigu.daijia.payment.config.WxPayV3Properties;
 import com.atguigu.daijia.payment.mapper.PaymentInfoMapper;
 import com.atguigu.daijia.payment.service.WxPayService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.wechat.pay.java.core.RSAAutoCertificateConfig;
+import com.wechat.pay.java.service.payments.jsapi.JsapiServiceExtension;
+import com.wechat.pay.java.service.payments.jsapi.model.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @Slf4j
 public class WxPayServiceImpl implements WxPayService {
+
+    @Autowired
+    private PaymentInfoMapper paymentInfoMapper;
+
+    @Autowired
+    private RSAAutoCertificateConfig rsaAutoCertificateConfig;
+
+    @Autowired
+    private WxPayV3Properties wxPayV3Properties;
+    @Override
+    public WxPrepayVo createWxPayment(PaymentInfoForm paymentInfoForm) {
+
+        try {
+            // 1.添加支付记录到支付表里
+            // 判断：如果表中存在订单支付记录，不需要添加
+            PaymentInfo paymentInfo = paymentInfoMapper.selectOne(new LambdaQueryWrapper<PaymentInfo>().eq(PaymentInfo::getOrderNo, paymentInfoForm.getOrderNo()));
+            if(null == paymentInfo) {
+                paymentInfo = new PaymentInfo();
+                BeanUtils.copyProperties(paymentInfoForm, paymentInfo);
+                paymentInfo.setPaymentStatus(0);
+                paymentInfoMapper.insert(paymentInfo);
+            }
+            // 2.创建微信支付使用对象 构建service
+            JsapiServiceExtension service = new JsapiServiceExtension.Builder().config(rsaAutoCertificateConfig).build();
+
+            // 3.request.setXxx(val)设置所需参数，具体参数可见Request定义
+            PrepayRequest request = new PrepayRequest();
+            Amount amount = new Amount();
+            amount.setTotal(paymentInfoForm.getAmount().multiply(new BigDecimal(100)).intValue());
+            request.setAmount(amount);
+            request.setAppid(wxPayV3Properties.getAppid());
+            request.setMchid(wxPayV3Properties.getMerchantId());
+            //string[1,127]
+            String description = paymentInfo.getContent();
+            if(description.length() > 127) {
+                description = description.substring(0, 127);
+            }
+            request.setDescription(paymentInfo.getContent());
+            request.setNotifyUrl(wxPayV3Properties.getNotifyUrl());
+            request.setOutTradeNo(paymentInfo.getOrderNo());
+
+            //获取用户信息
+            Payer payer = new Payer();
+            payer.setOpenid(paymentInfoForm.getCustomerOpenId());
+            request.setPayer(payer);
+
+            //是否指定分账，不指定不能分账
+            SettleInfo settleInfo = new SettleInfo();
+            settleInfo.setProfitSharing(true);
+            request.setSettleInfo(settleInfo);
+
+            // 4.调用微信支付 使用对象里面方法实现微信支付调用
+            // 调用下单方法，得到应答
+            // response包含了调起支付所需的所有参数，可直接用于前端调起支付
+            PrepayWithRequestPaymentResponse response = service.prepayWithRequestPayment(request);
+            log.info("微信支付下单返回参数：{}", JSON.toJSONString(response));
+            // 5.返回结果，封装到WxPrepayVo里面
+            WxPrepayVo wxPrepayVo = new WxPrepayVo();
+            BeanUtils.copyProperties(response, wxPrepayVo);
+            wxPrepayVo.setTimeStamp(response.getTimeStamp());
+            return wxPrepayVo;
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+    }
 
 
 }
