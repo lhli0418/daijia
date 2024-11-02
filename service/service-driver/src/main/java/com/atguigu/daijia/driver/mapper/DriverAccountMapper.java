ac8e644da339945924d94e5fc00573e344376e77
lehang <1430081225@qq.com>
2024-11-02 20:27:04 +0800

    8.4.3.3 mybatis-plus 规则配置

--- service/service-driver/src/main/java/com/atguigu/daijia/driver/mapper/DriverAccountMapper.java
+++ service/service-driver/src/main/java/com/atguigu/daijia/driver/mapper/DriverAccountMapper.java
@@ -4,8 +4,16 @@ import com.atguigu.daijia.model.entity.driver.DriverAccount;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.math.BigDecimal;
+
 @Mapper
 public interface DriverAccountMapper extends BaseMapper<DriverAccount> {
 
-
+    /**
+     * 系统奖励打入司机账户
+     * @param driverId
+     * @param amount
+     * @return
+     */
+    Integer add(Long driverId, BigDecimal amount);
 }
