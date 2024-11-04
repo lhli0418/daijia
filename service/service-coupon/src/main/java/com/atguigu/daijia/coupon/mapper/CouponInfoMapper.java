a0ef4403c151d843be870298db5050810f0ef976
lehang <1430081225@qq.com>
2024-11-04 17:03:19 +0800

    9.1.3.1 优惠券 优惠券领取 查找已使用优惠券 优惠券微服务接口

--- service/service-coupon/src/main/java/com/atguigu/daijia/coupon/mapper/CouponInfoMapper.java
+++ service/service-coupon/src/main/java/com/atguigu/daijia/coupon/mapper/CouponInfoMapper.java
@@ -3,6 +3,7 @@ package com.atguigu.daijia.coupon.mapper;
 import com.atguigu.daijia.model.entity.coupon.CouponInfo;
 import com.atguigu.daijia.model.vo.coupon.NoReceiveCouponVo;
 import com.atguigu.daijia.model.vo.coupon.NoUseCouponVo;
+import com.atguigu.daijia.model.vo.coupon.UsedCouponVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -15,4 +16,6 @@ public interface CouponInfoMapper extends BaseMapper<CouponInfo> {
     IPage<NoReceiveCouponVo> findNoReceivePage(Page<CouponInfo> pageParam, @Param("customerId") Long customerId);
 
     IPage<NoUseCouponVo> findNoUsePage(Page<CouponInfo> pageParam, Long customerId);
+
+    IPage<UsedCouponVo> findUsedPage(Page<CouponInfo> pageParam, Long customerId);
 }
--- service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/CouponInfoService.java
+++ service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/CouponInfoService.java
@@ -5,6 +5,7 @@ import com.atguigu.daijia.model.entity.order.OrderInfo;
 import com.atguigu.daijia.model.vo.base.PageVo;
 import com.atguigu.daijia.model.vo.coupon.NoReceiveCouponVo;
 import com.atguigu.daijia.model.vo.coupon.NoUseCouponVo;
+import com.atguigu.daijia.model.vo.coupon.UsedCouponVo;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -14,4 +15,6 @@ public interface CouponInfoService extends IService<CouponInfo> {
     PageVo<NoReceiveCouponVo> findNoReceivePage(Page<CouponInfo> pageParam, Long customerId);
 
     PageVo<NoUseCouponVo> findNoUsePage(Page<CouponInfo> pageParam, Long customerId);
+
+    PageVo<UsedCouponVo> findUsedPage(Page<CouponInfo> pageParam, Long customerId);
 }
