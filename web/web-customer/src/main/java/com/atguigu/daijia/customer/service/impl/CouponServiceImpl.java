463e64797fe223d053633781be973f3edebec1ae
lehang <1430081225@qq.com>
2024-11-04 16:54:46 +0800

    9.1.2.3 优惠券 优惠券领取 查找未使用优惠券 乘客端web接口

--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CouponServiceImpl.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CouponServiceImpl.java
@@ -4,6 +4,7 @@ import com.atguigu.daijia.coupon.client.CouponFeignClient;
 import com.atguigu.daijia.customer.service.CouponService;
 import com.atguigu.daijia.model.vo.base.PageVo;
 import com.atguigu.daijia.model.vo.coupon.NoReceiveCouponVo;
+import com.atguigu.daijia.model.vo.coupon.NoUseCouponVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,17 @@ public class CouponServiceImpl implements CouponService {
     @Override
     public PageVo<NoReceiveCouponVo> findNoReceivePage(Long customerId, Long page, Long limit) {
         return couponFeignClient.findNoReceivePage(customerId,page,limit).getData();
+    }
+
+    /**
+     * 查询未使用优惠券分页列表
+     * @param customerId
+     * @param page
+     * @param limit
+     * @return
+     */
+    @Override
+    public PageVo<NoUseCouponVo> findNoUsePage(Long customerId, Long page, Long limit) {
+        return couponFeignClient.findNoUsePage(customerId, page, limit).getData();
     }
 }
